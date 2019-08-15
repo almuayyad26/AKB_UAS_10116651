bc375a6807d73abd1bd32ffadfb73f806fb93c99
Muhamad Iyad Muayyad <49977759+almuayyad26@users.noreply.github.com>
2019-08-15 22:58:10 +0700

    Update fKontak.java

--- app/src/main/java/com/mim/akb_10116651/view/fKontak.java
+++ app/src/main/java/com/mim/akb_10116651/view/fKontak.java
@@ -12,6 +12,10 @@ import androidx.fragment.app.Fragment;
 import com.mim.akb_10116651.R;
 
 public class fKontak extends Fragment {
+//    09/08/2019
+//    NIM:10116651
+//    Nama:Muhamad Iyad Muayyad
+//    Kelas:IF-13
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
