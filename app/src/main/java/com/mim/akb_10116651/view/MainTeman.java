b8f6539e5da554b5aace9c64cd0c950455f3663e
Muhamad Iyad Muayyad <49977759+almuayyad26@users.noreply.github.com>
2019-08-15 22:56:23 +0700

    Update MainTeman.java

--- app/src/main/java/com/mim/akb_10116651/view/MainTeman.java
+++ app/src/main/java/com/mim/akb_10116651/view/MainTeman.java
@@ -5,5 +5,9 @@ import com.mim.akb_10116651.model.model_teman;
 import java.util.List;
 
 public interface MainTeman {
+//    11/08/2019
+//    NIM:10116651
+//    Nama:Muhamad Iyad Muayyad
+//    Kelas:IF-13
     List<model_teman> dataDummy();
 }
