714be856a4c486d2d8e06cdfeb6029b61e385737
Muhamad Iyad Muayyad <49977759+almuayyad26@users.noreply.github.com>
2019-08-15 22:54:20 +0700

    Update MainPresenter.java

--- app/src/main/java/com/mim/akb_10116651/presenter/MainPresenter.java
+++ app/src/main/java/com/mim/akb_10116651/presenter/MainPresenter.java
@@ -1,6 +1,10 @@
 package com.mim.akb_10116651.presenter;
 
 public interface MainPresenter {
+//    09/08/2019
+//    NIM:10116651
+//    Nama:Muhamad Iyad Muayyad
+//    Kelas:IF-13
     void updateNama();
     void updateKelas(String kelas);
     void updateNim(String nim);
